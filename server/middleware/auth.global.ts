6c2f6086a43dcf121637fb4f98d266db2f065269
sorrysatori <antosmatej@gmail.com>
2025-08-27 13:35:48 +0200

    fix: auth

--- server/middleware/auth.global.ts
+++ server/middleware/auth.global.ts
@@ -1,14 +1,35 @@
+import { defineEventHandler, getCookie, setCookie, createError } from 'h3'
+
 export default defineEventHandler((event) => {
-    const auth = getHeader(event, "authorization");
-  
-    const username = "kabypara";
-    const password = "TajnyHeslo666";
-    const basicAuth = "Basic " + Buffer.from(`${username}:${password}`).toString("base64");
-  
-    if (auth !== basicAuth) {
-      setResponseStatus(event, 401);
-      setHeader(event, "WWW-Authenticate", "Basic realm='Secure Area'");
-      return "Authentication required";
-    }
-  });
-  
+  const session = getCookie(event, 'auth')
+  if (session === 'ok') {
+    return
+  }
+
+  const auth = event.node.req.headers['authorization']
+  if (!auth) {
+    event.node.res.setHeader('WWW-Authenticate', 'Basic realm="Restricted Area"')
+    throw createError({ statusCode: 401, statusMessage: 'Auth Required' })
+  }
+
+  const [scheme, encoded] = auth.split(' ')
+  if (scheme !== 'Basic') {
+    throw createError({ statusCode: 400, statusMessage: 'Invalid Auth Scheme' })
+  }
+
+  const [user, pass] = Buffer.from(encoded, 'base64').toString().split(':')
+
+  if (user === process.env.NUXT_BASIC_USER && pass === process.env.NUXT_BASIC_PASS) {
+    setCookie(event, 'auth', 'ok', {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+      path: '/',
+      maxAge: 60 * 60 * 24 // 1 den
+    })
+    return
+  }
+
+  event.node.res.setHeader('WWW-Authenticate', 'Basic realm="Restricted Area"')
+  throw createError({ statusCode: 401, statusMessage: 'Invalid Credentials' })
+})
